e84a0d917f3657f6a001c2049377b2e99fe7f0a1
KaelvenSC <basithimana@gmail.com>
2026-01-22 21:07:39 +0700

    Update fishit.lua

--- fishit.lua
+++ fishit.lua
@@ -1,6 +1,6 @@
 _G.FishItConfig = _G.FishItConfig or {
     ["Fishing"] = {
-        ["Auto Perfect"] = false,
+        ["Auto Perfect"] = true,
         ["Random Result"] = false,
 
         ["Auto Favorite"] = true,
