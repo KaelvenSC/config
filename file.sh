c87c25d9d9812ce19bb6218871f4409045a94869
KaelvenSC <basithimana@gmail.com>
2026-03-08 00:28:49 +0700

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,1 +1,29 @@
-termux-setup-storage && mkdir -p /storage/emulated/0/Delta/Autoexecute && curl -L -o /storage/emulated/0/Delta/Autoexecute/script.lua https://raw.githubusercontent.com/USERNAME/REPO/main/script.lua
+#!/bin/bash
+
+echo "https://gofile.io/d/X3BaAy"
+read LINK
+
+ID=$(echo $LINK | awk -F/ '{print $NF}')
+
+DATA=$(curl -s https://api.gofile.io/contents/$ID)
+
+echo "Daftar file:"
+echo "$DATA" | grep -o '"name":"[^"]*"' | cut -d':' -f2 | tr -d '"' | nl
+
+echo "Pilih range file (contoh 1-5):"
+read RANGE
+
+START=$(echo $RANGE | cut -d- -f1)
+END=$(echo $RANGE | cut -d- -f2)
+
+LINKS=$(echo "$DATA" | grep -o '"link":"[^"]*"' | cut -d':' -f2- | tr -d '"')
+
+COUNT=1
+for URL in $LINKS
+do
+if [ $COUNT -ge $START ] && [ $COUNT -le $END ]; then
+echo "Downloading $COUNT..."
+wget $URL
+fi
+COUNT=$((COUNT+1))
+done
